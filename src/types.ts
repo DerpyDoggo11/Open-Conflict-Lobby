ccd7e683a938c11411beb1fd5c368d6946406814
DerpyDoggo11 <vrs18451@gmail.com>
2026-03-22 10:45:34 -0700

    init

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+
+export type PageName = 'home' | 'servers' | 'loadout' | 'settings';
+
+export type NavigateFn = (page: PageName) => void;
+
+export interface Server {
+    id: number;
+    name: string;
+    currentPlayers: number;
+    maxPlayers: number;
+}
+
+export interface MapOption {
+  id: string;
+  label: string;
+}
